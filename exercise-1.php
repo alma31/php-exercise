2bb45150bb77699af31db063cfbba1bdef5d6d3f
alma31 <lenni31@neuf.fr>
2017-01-09 14:02:37 +0100

    exercise 1 final

---
+++ exercise-1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exercise 1</title>
+</head>
+<body>
+
+<?php
+
+for ($i = 0; $i <= 10; $i++){
+	echo $i;
+}
+
+  ?>
+	
+</body>
+</html>
