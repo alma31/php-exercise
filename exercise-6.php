c29c2248133de2ef0ddef7856c9d6c3f9aa1526c
alma31 <lenni31@neuf.fr>
2017-01-09 15:42:19 +0100

    exercise 6 fini

---
+++ exercise-6.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exercise 6</title>
+</head>
+<body>
+	<?php  
+
+	for ($i=20; $i > 0; $i--) { 
+		echo $i."C'est presque bon</br>";
+	}
+
+	?>
+</body>
+</html>
