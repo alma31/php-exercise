93fa9bc0da0642dcb5dfcccc461dc65cafbb389a
alma31 <lenni31@neuf.fr>
2017-01-09 16:46:56 +0100

    exercise 1 function fini

---
+++ Function/exercise-1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exercise 1</title>
+</head>
+<body>
+<?php  
+
+function test(){
+
+	return true;
+}
+
+echo test();
+
+?>
+	
+</body>
+</html>
