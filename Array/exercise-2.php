95c0206e685606e32a6bd36f3c45ead4aa2e4d38
alma31 <lenni31@neuf.fr>
2017-01-10 12:20:05 +0100

    exercise 2 fini

---
+++ Array/exercise-2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exercise 2</title>
+</head>
+<body>
+	<?php 
+	$mois = [
+	"janvier",
+	"février",
+	"mars",
+	"avril",
+	"mai",
+	"juin",
+	"juillet",
+	"aout",
+	"septembre",
+	"octobre",
+	"novembre",
+	"décembre",
+	];
+	echo ($mois[2]);
+	?>
+</body>
+</html>
