b0f3b5bfe7b185c458a68e07b7cc3722101e1e94
alma31 <lenni31@neuf.fr>
2017-01-10 16:49:01 +0100

    exercise 3 fini

---
+++ Form/exercise-3/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exercise 1</title>
+</head>
+<body>
+	<form action="user.php" method="get">
+		<input type="text" name="nom">
+		<input type="text" name="prenom">
+		<input type="submit" name="validé">
+	</form>
+</body>
+</html>
---
+++ Form/exercise-3/user.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exercise 1</title>
+</head>
+<body>
+	<?php 
+
+	echo $_GET['prenom'] . ' ' . $_GET['nom'];
+
+	 ?>
+</body>
+</html>
