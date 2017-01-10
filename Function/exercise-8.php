d5bb844f33ea1313e38515a76a8b3b0db321c6fe
alma31 <lenni31@neuf.fr>
2017-01-10 11:48:50 +0100

    exercise 8 fini

---
+++ Function/exercise-8.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exercise 8</title>
+</head>
+<body>
+	
+		<?php  
+
+		function test($a,$b,$c){
+
+			return $a+$b+$c;
+
+		}
+
+		echo test(2,20,20);
+
+		?>
+	
+</body>
+</html>
