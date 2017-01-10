043d9acd394d1efae83a1da17345a52c2db7d9bb
alma31 <lenni31@neuf.fr>
2017-01-10 14:04:19 +0100

    exercise 7 fini

---
+++ Array/exercise-7.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exercise 7</title>
+</head>
+<body>
+	<?php 
+
+	$hdf = [
+
+	"02" => "Aisne",
+	"59" => "Nord",
+	"60" => "Oise",
+	"62" => "Pas-de-Calais",
+	"80" => "Somme",
+	];
+
+	$hdf["51"] = "Marne";
+
+	?>
+</body>
+</html>
