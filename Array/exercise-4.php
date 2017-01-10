c58e783aa46da43abfc665536f11bf48eacc85ce
alma31 <lenni31@neuf.fr>
2017-01-10 12:29:39 +0100

    exercise 4 fini

---
+++ Array/exercise-4.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exercise 4</title>
+</head>
+<body>
+	<?php 
+
+	$mois = [
+	"janvier",
+	"février",
+	"mars",
+	"avril",
+	"mai",
+	"juin",
+	"juillet",
+	"août",
+	"septembre",
+	"octobre",
+	"novembre",
+	"décembre",
+	];
+	echo ($mois[7]);
+
+
+	?>
+</body>
+</html>
