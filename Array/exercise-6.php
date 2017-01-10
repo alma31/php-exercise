6cd8b5724bc492224755d85f70437cf66be01b5b
alma31 <lenni31@neuf.fr>
2017-01-10 13:55:16 +0100

    exercise 6 fini

---
+++ Array/exercise-6.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exercise 6</title>
+</head>
+<body>
+	<?php 
+
+	$hdf = [
+
+	"02" => "Aisne",
+	"59" => "Nord",
+	"60" => "Oise",
+	"62" => "Pas-de-Calais",
+	"80" => "Somme",
+	];
+
+	echo $hdf["59"];
+	?>
+</body>
+</html>
