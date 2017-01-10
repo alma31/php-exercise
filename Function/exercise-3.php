15fc33a0407b4970e655fb9212e08baee8704919
alma31 <lenni31@neuf.fr>
2017-01-10 10:44:23 +0100

    exercise 3 fini

---
+++ Function/exercise-3.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exercise 3</title>
+</head>
+<body>
+
+	<?php 
+
+	function test($a = "ma premiere chaine de carractere", $b = "ma deuxieme chaine de carractere"){
+
+		return $a." ".$b;
+	}
+
+	echo test();
+
+	?>
+
+	
+</body>
+</html>
