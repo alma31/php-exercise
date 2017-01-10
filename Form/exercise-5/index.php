27cd329d6a1f1b06945008547ff5a11d371cc228
alma31 <lenni31@neuf.fr>
2017-01-10 17:00:54 +0100

    exercise 5 fini

---
+++ Form/exercise-5/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exercise 5</title>
+</head>
+<body>
+	<form action="index.php" method="post">
+	<select name="choix">
+		<option>Mr</option>
+		<option>Mme</option>
+	</select>
+		<input type="text" name="nom">
+		<input type="text" name="prenom">
+		<input type="submit" name="validé">
+	</form>
+</body>
+</html>
