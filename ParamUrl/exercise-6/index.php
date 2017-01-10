19bd2c1b62cfef7d83c9016e8d13fec093397b3b
alma31 <lenni31@neuf.fr>
2017-01-10 16:09:23 +0100

    exercise 6 fini

---
+++ ParamUrl/exercise-6/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exercise 6</title>
+</head>
+<body>
+	<?php
+
+	echo $_GET['batiment'] . ' ' . $_GET['salle'];
+
+	  ?>
+</body>
+</html>
