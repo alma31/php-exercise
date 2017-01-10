1b5e4fb549571d3ec441c708d3850749fc28c387
alma31 <lenni31@neuf.fr>
2017-01-10 16:53:43 +0100

    exercise 4 fini

---
+++ Form/exercise-4/user.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exercise 4</title>
+</head>
+<body>
+	<?php 
+
+	echo $_POST['prenom'] . ' ' . $_POST['nom'];
+
+	 ?>
+</body>
+</html>
