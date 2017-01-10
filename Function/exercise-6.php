d895629779c816585fb34667b32257262fd026fe
alma31 <lenni31@neuf.fr>
2017-01-10 11:14:57 +0100

    exercise 6 fini

---
+++ Function/exercise-6.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exercise 6</title>
+</head>
+<body>
+	
+	<?php  
+
+	function test($nom = "Lacombe", $prenom = "Lenni", $age = 23){
+
+		echo "bonjour"." ".$nom." ".$prenom.", tu a"." ".$age." "."ans.";
+	}
+
+	test();
+
+	?>
+	
+</body>
+</html>
