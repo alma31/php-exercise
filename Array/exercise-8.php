f3d7169f3f312061841c16ff103348f131aa1721
alma31 <lenni31@neuf.fr>
2017-01-10 14:18:11 +0100

    exercise 8 fini

---
+++ Array/exercise-8.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exercise 8</title>
+</head>
+<body>
+	<?php 
+
+	$mois = [
+	"janvier",
+	"février",
+	"mars",
+	"avril",
+	"mai",
+	"juin",
+	"juillet",
+	"aout",
+	"septembre",
+	"octobre",
+	"novembre",
+	"décembre",
+	];
+
+	for ($i=0; $i < 12; $i++) { 
+		
+		echo $mois[$i].'<br />';
+	}
+
+
+
+	?>
+</body>
+</html>
