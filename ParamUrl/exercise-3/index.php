f4c72de929bc6ebb8e823ffecbd61027d8feae0b
alma31 <lenni31@neuf.fr>
2017-01-10 15:59:10 +0100

    exercise 3 fini

---
+++ ParamUrl/exercise-3/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exercise 3</title>
+</head>
+<body>
+	<?php 
+
+	echo $_GET['dateDebut'] . ' ' . $_GET['dateFin'];
+
+	 ?>
+</body>
+</html>
