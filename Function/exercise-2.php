7fff2400fdd3f835ab1494ddfafdb26ad6d729bc
alma31 <lenni31@neuf.fr>
2017-01-10 09:50:34 +0100

    exercise 2 fini

---
+++ Function/exercise-2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exercise 2</title>
+</head>
+<body>
+	<?php 
+
+	function test($test = "ceci et un test"){
+
+		return $test;
+
+	}
+
+	echo test();
+
+
+	 ?>
+</body>
+</html>
