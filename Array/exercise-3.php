9232969a0316f7e217a992c231bc0ac5bc332306
alma31 <lenni31@neuf.fr>
2017-01-10 12:25:39 +0100

    exercise 3 fini

---
+++ Array/exercise-3.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exercise 3</title>
+</head>
+<body>
+	<?php 
+	$mois = [
+	"janvier",
+	"février",
+	"mars",
+	"avril",
+	"mai",
+	"juin",
+	"juillet",
+	"aout",
+	"septembre",
+	"octobre",
+	"novembre",
+	"décembre",
+	];
+	echo ($mois[5]);
+	 ?>
+</body>
+</html>
