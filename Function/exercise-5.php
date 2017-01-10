0a70c5ac21fc00fce170110e9876eecd9e80a741
alma31 <lenni31@neuf.fr>
2017-01-10 10:59:05 +0100

    exercise 5 fini

---
+++ Function/exercise-5.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exercise 5</title>
+</head>
+<body>
+	<?php 
+
+	function test($a = 5, $b = "c'est ma chaine de carractere"){
+
+		return $a." ".$b;
+	}
+
+	echo test();
+
+	 ?>
+</body>
+</html>
