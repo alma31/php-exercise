6058af0db1c9f6bbc7620746d3f24e4250d11b61
alma31 <lenni31@neuf.fr>
2017-01-11 10:47:19 +0100

    exercise 7 fini

---
+++ Form/exercise-7/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exercise 7</title>
+</head>
+<body>  
+	<?php 
+	if (empty($_POST)) {
+		?>
+		<form action="index.php" method="post">
+			<select name="choix">
+				<option>Mr</option>
+				<option>Mme</option>
+			</select>
+			<input type="text" name="nom">
+			<input type="text" name="prenom">
+			<input type="file" name="fichier">
+			<input type="submit" name="validé">
+		</form>
+		<?php 
+	}else{
+		if (isset($_POST["choix"]) && isset($_POST["prenom"]) && isset($_POST["nom"]) && isset($_POST["fichier"])) {
+			echo "Bienvenue " . $_POST["choix"] ." ". $_POST["prenom"] ." ". $_POST["nom"] . "!" . " ".$_POST["fichier"] ;
+		}else{
+			echo "Veuiller remplir tous les champs, merci.";
+		}
+	}
+	?>
+
+
+</body>
+</html>
