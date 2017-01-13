4821893ab86d35bb7e3a33b2613509bc292641cf
alma31 <lenni31@neuf.fr>
2017-01-13 12:25:06 +0100

    exercise 3 fini

---
+++ date/exercise-3/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exercise 3</title>
+</head>
+<body>
+	<?php 
+
+	date_default_timezone_set('Europe/Paris');
+	echo date('l d F Y');
+	 ?>
+</body>
+</html>
