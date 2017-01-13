3853feb26c060b3b5936175c391830f5608db2ce
alma31 <lenni31@neuf.fr>
2017-01-13 12:07:58 +0100

    exercise 2 fini

---
+++ date/exercise-2/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exercise 2</title>
+</head>
+<body>
+	<?php 
+
+	date_default_timezone_set('UTC');
+	echo date('d-m-y');
+	 ?>
+</body>
+</html>
